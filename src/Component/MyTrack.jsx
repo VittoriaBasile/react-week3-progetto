ae55f53aeed2957d51307292efc8d1b8751d570d
VittoriaBasile <vichy.basile@gmail.com>
2023-04-14 19:51:49 +0200

    commit album main

--- src/Component/MyTrack.jsx
+++ src/Component/MyTrack.jsx
@@ -1,21 +1,20 @@
 import { Col, Container, Row } from "react-bootstrap";
-import { Link } from "react-router-dom";
 
 const MyTrack = ({ track }) => {
   return (
     <Container>
-      <Row>
-        <Col xs={10} className="text-light offset">
-          <p>{track.title}</p>
+      <div className="d-flex justify-content-between align-items-center gap-5">
+        <div className="text-light">
+          <p className="fw-bold">{track.title}</p>
+          <p className="text-light">{track.artist.name}</p>
+        </div>
 
-          <p>{track.artist.name}</p>
-        </Col>
-        <Col className="text-light ms-1">
-          <p>
-            {Math.floor(track.duration / 60)}:{(track.duration % 60).toString().padStart(2, "0")}s
+        <div>
+          <p className="text-light">
+            {Math.floor(track.duration / 60)}:{(track.duration % 60).toString().padStart(2, "0")}
           </p>
-        </Col>
-      </Row>
+        </div>
+      </div>
     </Container>
   );
 };
